738836fc18867a81e00322ecea7fd11efa0f0295
MiguelConcha <mconcha@ciencias.unam.mx>
2018-02-22 19:37:49 -0600

    Datos básicos añadidos.

---
+++ Clasificación de Documentos/C4.5.py
@@ -0,0 +1,13 @@
+class Clase: 
+	def __init__(self, valor):
+		self.valor = valor
+
+class Atributo:
+	def __init__(self, nombre, clases):
+		self.nombre = nombre
+		for clase in clases:
+			self.lista_clases.append(clase)
+
+class Ejemplo:
+	def __init__(self, lista_atributos, lista_valores):
+		self.atr_val = dict(zip(lista_atributos, lista_valores)) 
